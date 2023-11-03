8b1cc48d4d3d4e1f26fb1de18300e678c018cc22
Risshiganesh Villalan <risshiganesh1997@gmail.com>
2023-11-03 08:41:57 +0800

    Complete gif generator

---
+++ script.js
@@ -0,0 +1,42 @@
+const searchbar = document.querySelector("#search-bar");
+const statusMsg = document.querySelector(".status-msg");
+const img = document.querySelector("img");
+const newImageBtn = document.querySelector(".new-image");
+const api_key = "6oy9SB3U4oOfolfXWyYz2IHrnTfmmYF8";
+// const api_key1 = "6oy9SB3U4oOfolfXWyYz2IHrnTfmmYF81111";
+
+function fetchNewImage() {
+  statusMsg.textContent = "";
+  fetch(
+    `https://api.giphy.com/v1/gifs/translate?api_key=${api_key}&s=${searchbar.value}`,
+    { mode: "cors" }
+  )
+    .then(function (response) {
+      const responseCopy = response.clone();
+      const responseParsed = responseCopy.json();
+
+      //   console.log(responseParsed);
+      return responseParsed;
+    })
+    .then(function (response) {
+      console.log(response.data);
+      if (response.data.length === 0) {
+        statusMsg.textContent = "GIF NOT FOUND!";
+        img.src = "";
+        console.log("GIF NOT FOUND");
+        return;
+      }
+      img.src = response.data.images.original.url;
+      console.log(response.meta.status);
+    })
+    .catch(function (error) {
+      //   console.log(`Error: ${error}`);
+      throw new Error(`Error: ${error}`);
+    });
+}
+
+fetchNewImage();
+newImageBtn.addEventListener("click", fetchNewImage);
+
+// ``;
+// `https://api.giphy.com/v1/gifs/translate?api_key=6oy9SB3U4oOfolfXWyYz2IHrnTfmmYF8&s=${searchbar.value}`;
