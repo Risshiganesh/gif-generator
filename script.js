6936babeb8eccfdd4e4f596e9359fe60e0b31c0a
Risshiganesh Villalan <risshiganesh1997@gmail.com>
2023-11-06 11:57:55 +0800

    Refactor with async/await

--- script.js
+++ script.js
@@ -5,39 +5,27 @@ const newImageBtn = document.querySelector(".search-button");
 const api_key = "6oy9SB3U4oOfolfXWyYz2IHrnTfmmYF8";
 
 async function fetchNewImage() {
-  statusMsg.textContent = "";
-  const response = await fetch(
-    `https://api.giphy.com/v1/gifs/translate?api_key=${api_key}&s=${searchbar.value}`,
-    { mode: "cors" }
-  );
-
-  const responseCopy = await response.clone();
-
-  const responseParsed = await responseCopy.json();
-
-  console.log(responseParsed);
-
-  Promise.resolve(responseParsed)
-
-    // .then(function (response) {
-    //   return response.json();
-    // })
-    .then(function (response) {
-      console.log(response);
-      console.log(response);
-      if (response.data.length === 0) {
-        statusMsg.textContent = "GIF NOT FOUND!";
-        img.src = "";
-        console.log("GIF NOT FOUND");
-        return;
-      }
-      img.src = response.data.images.original.url;
-      console.log(response.meta.status);
-    })
-    .catch(function (error) {
-      //   console.log(`Error: ${error}`);
-      throw new Error(`Error: ${error}`);
-    });
+  try {
+    statusMsg.classList.remove("status-active");
+    statusMsg.textContent = "";
+    const response = await fetch(
+      `https://api.giphy.com/v1/gifs/translate?api_key=${api_key}&s=${searchbar.value}`,
+      { mode: "cors" }
+    );
+    // const responseCopy = response.clone();
+    const gifData = await response.json();
+    console.log(gifData);
+    if (gifData.data.length === 0) {
+      statusMsg.classList.add("status-active");
+      statusMsg.textContent = "GIF NOT FOUND!";
+      img.src = "";
+      return;
+    }
+    img.src = gifData.data.images.original.url;
+    console.log(gifData.meta.status);
+  } catch (error) {
+    throw new Error(`ERROR: ${error}`);
+  }
 }
 
 fetchNewImage();
